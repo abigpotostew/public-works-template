027e98f5311557bb8995b8272b9b63670e372cd1
stew.bracken <stew.bracken@gmail.com>
2022-06-20 09:03:32 -0700

    click for new hash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import {PRNGRand} from "./random";
+import {generateTxHash, PRNGRand} from "./random";
 
 const isPreview = new URLSearchParams(window.location.search).get('preview') === 'true'
 const prng = new PRNGRand()
+
+// During development, refresh the hash on click.
+document.addEventListener('mouseup', (e) => {
+    window.location = '?hash=' + generateTxHash()
+})
