35e0eefc59749a8454a5599f1f9153a948308c5c
stew.bracken <stew.bracken@gmail.com>
2022-06-20 10:34:36 -0700

    previewReady

--- src/index.js
+++ src/index.js
@@ -7,3 +7,18 @@ const prng = new PRNGRand()
 document.addEventListener('mouseup', (e) => {
     window.location = '?hash=' + generateTxHash()
 })
+
+// Attributes are human-readable traits.
+window.attributes = {
+    "Color": prng.randomList(['Blue', 'Purple'])
+};
+
+// Traits are optional numeric attributes that represent the traits.
+window.traits = {
+    "ColorR": prng.randomInt(0, 255),
+    "ColorG": prng.randomInt(0, 255),
+    "ColorB": prng.randomInt(0, 255)
+};
+
+// When the preview is rendered, set this field to true.
+window.previewReady = true;
