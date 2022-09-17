82dbb80505d8ebabd915ff041b1fffd1f3e890d5
stew.bracken <stew.bracken@gmail.com>
2022-09-16 22:18:53 -0700

    Functional API

--- src/index.js
+++ src/index.js
@@ -1,41 +1,21 @@
-import {generateTxHash, PRNGRand} from "./random";
+import {createPrng, isPWPreview, registerDevEvents, setPreviewReady, setProperties} from "./publicworks";
 
-// isPreview is true when rending in the public works preview pipeline.
-const isPreview = new URLSearchParams(window.location.search).get('preview') === 'true'
-const prng = new PRNGRand()
+const prng = createPrng()
 
-// During development, refresh the hash on click.
-const registerDevEvents = ()=>{
-    const freshHash = ()=>{
-        window.location = '?hash=' + generateTxHash()
-    }
-    document.addEventListener('mouseup', (e) => {
-        freshHash()
-    })
-    window.addEventListener('touchend', function () {
-        freshHash()
-    });
-    document.addEventListener('keyup', (e) => {
-        if(e.key ===' '){
-            freshHash()
-        }
-    })
-}
+// optionally refresh hash on space bar
 registerDevEvents()
 
-// Attributes are human-readable traits and will appear on marketplaces and rarity sites.
-window.attributes = {
+setProperties({
     "Color": prng.randomList(['Blue', 'Purple'])
-};
-
-// Traits are optional numeric attributes that represent the raw values that create traits.
-window.traits = {
+}, {
     "ColorR": prng.randomInt(0, 255),
     "ColorG": prng.randomInt(0, 255),
     "ColorB": prng.randomInt(0, 255)
-};
+})
 
-// When the preview is rendered, set this field to true.
-if(isPreview) {
-    window.previewReady = true;
+// rendering logic...
+
+// When the preview is rendered, notify PW the preview is ready
+if (isPWPreview()) {
+    setPreviewReady()
 }
--- src/publicworks.js
+++ src/publicworks.js
@@ -1,3 +1,57 @@
+/**
+ * Get reset PRNG instance.
+ */
+export const createPrng = () => {
+    return new PRNGRand();
+}
+
+/**
+ * Check if the work is rendering in the public works preview pipeline
+ */
+export const isPWPreview = () => {
+    return new URLSearchParams(window.location.search).get('preview') === 'true'
+}
+
+/**
+ * Enable new hashes on click during development.
+ */
+export const registerDevEvents = () => {
+    const freshHash = () => {
+        window.location = '?hash=' + generateTxHash()
+    }
+    window.addEventListener('touchend', function () {
+        freshHash()
+    });
+    document.addEventListener('keyup', (e) => {
+        if (e.key === ' ') {
+            freshHash()
+        }
+    })
+}
+
+const setWindowProperties = (key, attributes) => {
+    if (typeof attributes !== 'object') {
+        throw new Error('attributes should be an object')
+    }
+    window[key] = {...attributes};
+}
+
+/**
+ * Set the NFT traits. Attributes are the NFT attributes seen on marketplaces. Traits are optional numeric
+ * representations of attributes to expose to public works.
+ */
+export const setProperties = (attributes, traits = {}) => {
+    setWindowProperties('attributes', attributes)
+    setWindowProperties('traits', traits)
+}
+
+/**
+ * Notify public works preview pipeline that rendering is complete.
+ */
+export const setPreviewReady = () => {
+    window.previewReady = true;
+}
+
 export const generateTxHash = () => {
     let alphabet = "0123456789ABCDEF"
     return Array(64).fill(0).map(_ => alphabet[(Math.random() * alphabet.length) | 0]).join('')
